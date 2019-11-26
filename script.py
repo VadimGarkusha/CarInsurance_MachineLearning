e9e12c7e7c97a8256f0a339d361b02f5462e0923
Vadym Harkusha <vadgig@gmail.com>
2019-11-26 00:16:33 -0500

    Added script for building the model

---
+++ script.py
@@ -0,0 +1,335 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+from scipy import stats
+from scipy.stats import skew
+from scipy.stats import mode
+from scipy.optimize import minimize
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier,GradientBoostingClassifier, VotingClassifier
+from sklearn.naive_bayes import GaussianNB
+import warnings
+warnings.filterwarnings('ignore')
+
+# Read-in train and test datasets
+train = pd.read_csv('/Users/vadimgarkusha/Desktop/College/Data Warehouse/data/carInsurance_train.csv')
+test = pd.read_csv('/Users/vadimgarkusha/Desktop/College/Data Warehouse/data/carInsurance_test.csv')
+
+print('1A')
+print('The train dataset has %d observations and %d features' % (train.shape[0], train.shape[1]))
+print('The test dataset has %d observations and %d features' % (test.shape[0], test.shape[1]))
+
+print('\n\nFirst 5 rows')
+print(train.head())
+print('\nShape of data')
+print(train.shape)
+print('\nData elements types')
+print(train.dtypes)
+
+print('\n\n1B')
+print('\nData description')
+print(train.describe())
+
+print('\n\n1D')
+# First check out correlations among numeric features
+# Heatmap is a useful tool to get a quick understanding of which variables are important
+colormap = plt.cm.viridis
+cor = train.corr()
+cor = cor.drop(['Id'],axis=1).drop(['Id'],axis=0)
+plt.figure(figsize=(10,10))
+hm = sns.heatmap(cor,vmax=0.8,cmap=colormap,annot=True,fmt='.2f',square=True,annot_kws={'size':9},linecolor='white',linewidths=0.1)
+bottom, top = hm.get_ylim()
+hm.set_ylim(bottom + 0.5, top - 0.5)
+#hm.get_figure().savefig('/Users/vadimgarkusha/Desktop/College/Data Warehouse/figures/heatmap.png', bbox_inches="tight")
+
+# Next, pair plot some important features
+imp_feats = ['CarInsurance','Age','Balance','HHInsurance', 'CarLoan','NoOfContacts','DaysPassed','PrevAttempts']
+fp = sns.pairplot(train[imp_feats],hue='CarInsurance',palette='viridis',size=2.5)
+#fp.savefig('/Users/vadimgarkusha/Desktop/College/Data Warehouse/figures/Feature_Pairs.png', bbox_inches="tight")
+
+# Take a further look at Age
+facet = sns.FacetGrid(train, hue='CarInsurance',size=5,aspect=3,palette='seismic')
+facet.map(plt.hist,'Age',bins=30,alpha=0.5,normed=True)
+facet.set(xlim=(0,train.Age.max()+10))
+facet.add_legend()
+#facet.savefig('/Users/vadimgarkusha/Desktop/College/Data Warehouse/figures/FacedDrid_Age.png', bbox_inches="tight")
+
+# Next check out categorical features
+cat_feats = train.select_dtypes(include=['object']).columns
+plt_feats = cat_feats[(cat_feats!= 'CallStart') & (cat_feats!='CallEnd')]
+
+for feature in plt_feats:
+    plt.figure(figsize=(10,6))
+    sns.barplot(feature,'CarInsurance', data=train,palette='Set2') 
+    
+# Check outliers
+# From the pairplot, we can see there is an outlier with extreme high balance. Drop that obs here.
+train[train['Balance']>80000]
+train = train.drop(train[train.index==1742].index)
+
+print('\n\n2A and 1C')
+print('Missing data analysis and evaluation')
+# merge train and test data here in order to impute missing values all at once
+all=pd.concat([train,test],keys=('train','test'))
+all.drop(['CarInsurance','Id'],axis=1,inplace=True)
+print(all.shape)
+
+total = all.isnull().sum()
+pct = total/all.isnull().count()
+NAs = pd.concat([total,pct],axis=1,keys=('Total','Pct'))
+NAs[NAs.Total>0].sort_values(by='Total',ascending=False)
+
+all_df = all.copy()
+
+# Fill missing outcome as not in previous campaign
+all_df[all_df['DaysPassed']==-1].count()
+all_df.loc[all_df['DaysPassed']==-1,'Outcome']='NoPrev'
+
+# Fill missing communication with none 
+all_df['Communication'].value_counts()
+all_df['Communication'].fillna('None',inplace=True)
+
+# Fill missing education with the most common education level by job type
+all_df['Education'].value_counts()
+
+# Create job-education level mode mapping
+edu_mode=[]
+job_types = all_df.Job.value_counts().index
+for job in job_types:
+    mode = all_df[all_df.Job==job]['Education'].value_counts().nlargest(1).index
+    edu_mode = np.append(edu_mode,mode)
+edu_map=pd.Series(edu_mode,index=all_df.Job.value_counts().index)
+
+# Apply the mapping to missing eductaion obs
+for j in job_types:
+    all_df.loc[(all_df['Education'].isnull()) & (all_df['Job']==j),'Education'] = edu_map.loc[edu_map.index==j][0]
+all_df['Education'].fillna('None',inplace=True)
+
+# Fill missing job with none
+all_df['Job'].fillna('None',inplace=True)
+
+# Double check if there is still any missing value
+all_df.isnull().sum().sum()
+
+
+print('\n\n2A Data Standardization and normalization')
+# First simplify some client features
+
+# Create age group based on age bands
+all_df['AgeBand']=pd.cut(all_df['Age'],5)
+print(all_df['AgeBand'].value_counts())
+
+all_df.loc[(all_df['Age']>=17) & (all_df['Age']<34),'AgeBin'] = 1
+all_df.loc[(all_df['Age']>=34) & (all_df['Age']<49),'AgeBin'] = 2
+all_df.loc[(all_df['Age']>=49) & (all_df['Age']<65),'AgeBin'] = 3
+all_df.loc[(all_df['Age']>=65) & (all_df['Age']<80),'AgeBin'] = 4
+all_df.loc[(all_df['Age']>=80) & (all_df['Age']<96),'AgeBin'] = 5
+all_df['AgeBin'] = all_df['AgeBin'].astype(int)
+
+# Create balance groups
+all_df['BalanceBand']=pd.cut(all_df['Balance'],5)
+print(all_df['BalanceBand'].value_counts())
+all_df.loc[(all_df['Balance']>=-3200) & (all_df['Balance']<17237),'BalanceBin'] = 1
+all_df.loc[(all_df['Balance']>=17237) & (all_df['Balance']<37532),'BalanceBin'] = 2
+all_df.loc[(all_df['Balance']>=37532) & (all_df['Balance']<57827),'BalanceBin'] = 3
+all_df.loc[(all_df['Balance']>=57827) & (all_df['Balance']<78122),'BalanceBin'] = 4
+all_df.loc[(all_df['Balance']>=78122) & (all_df['Balance']<98418),'BalanceBin'] = 5
+all_df['BalanceBin'] = all_df['BalanceBin'].astype(int)
+
+all_df = all_df.drop(['AgeBand','BalanceBand','Age','Balance'],axis=1)
+
+# Convert education level to numeric 
+all_df['Education'] = all_df['Education'].replace({'None':0,'primary':1,'secondary':2,'tertiary':3})
+
+print('\n\n2B')
+
+# Get call length
+all_df['CallEnd'] = pd.to_datetime(all_df['CallEnd'])
+all_df['CallStart'] = pd.to_datetime(all_df['CallStart'])
+all_df['CallLength'] = ((all_df['CallEnd'] - all_df['CallStart'])/np.timedelta64(1,'m')).astype(float)
+all_df['CallLenBand']=pd.cut(all_df['CallLength'],5)
+print(all_df['CallLenBand'].value_counts())
+
+# Create call length bins
+all_df.loc[(all_df['CallLength']>= 0) & (all_df['CallLength']<11),'CallLengthBin'] = 1
+all_df.loc[(all_df['CallLength']>=11) & (all_df['CallLength']<22),'CallLengthBin'] = 2
+all_df.loc[(all_df['CallLength']>=22) & (all_df['CallLength']<33),'CallLengthBin'] = 3
+all_df.loc[(all_df['CallLength']>=33) & (all_df['CallLength']<44),'CallLengthBin'] = 4
+all_df.loc[(all_df['CallLength']>=44) & (all_df['CallLength']<55),'CallLengthBin'] = 5
+all_df['CallLengthBin'] = all_df['CallLengthBin'].astype(int)
+all_df = all_df.drop('CallLenBand',axis=1)
+
+# Get call start hour
+all_df['CallStartHour'] = all_df['CallStart'].dt.hour
+print(all_df[['CallStart','CallEnd','CallLength','CallStartHour']].head())
+
+# Get workday of last contact based on call day and month, assuming the year is 2016
+all_df['LastContactDate'] = all_df.apply(lambda x:datetime.datetime.strptime("%s %s %s" %(2016,x['LastContactMonth'],x['LastContactDay']),"%Y %b %d"),axis=1)
+all_df['LastContactWkd'] = all_df['LastContactDate'].dt.weekday
+all_df['LastContactWkd'].value_counts()
+all_df['LastContactMon'] = all_df['LastContactDate'].dt.month
+all_df = all_df.drop('LastContactMonth',axis=1)
+
+# Get week of last contact
+all_df['LastContactWk'] = all_df['LastContactDate'].dt.week
+
+# Get num of week in a month. There might be easier ways to do this, I will keep exploring. 
+MonWk = all_df.groupby(['LastContactWk','LastContactMon'])['Education'].count().reset_index()
+MonWk = MonWk.drop('Education',axis=1)
+MonWk['LastContactWkNum']=0
+for m in range(1,13):
+    k=0
+    for i,row in MonWk.iterrows():
+        if row['LastContactMon']== m:
+            k=k+1
+            row['LastContactWkNum']=k
+            
+def get_num_of_week(df):
+    for i,row in MonWk.iterrows():
+        if (df['LastContactWk']== row['LastContactWk']) & (df['LastContactMon']== row['LastContactMon']):
+            return row['LastContactWkNum']
+
+all_df['LastContactWkNum'] = all_df.apply(lambda x: get_num_of_week(x),axis=1)
+print(all_df[['LastContactWkNum','LastContactWk','LastContactMon']].head(10))
+
+# Spilt numeric and categorical features
+cat_feats = all_df.select_dtypes(include=['object']).columns
+num_feats = all_df.select_dtypes(include=['float64','int64']).columns
+num_df = all_df[num_feats]
+cat_df = all_df[cat_feats]
+print('There are %d numeric features and %d categorical features\n' %(len(num_feats),len(cat_feats)))
+print('Numeric features:\n',num_feats.values)
+print('Categorical features:\n',cat_feats.values)
+
+# One hot encoding
+cat_df = pd.get_dummies(cat_df)
+
+# Merge all features
+all_data = pd.concat([num_df,cat_df],axis=1)
+
+print('\n\n2C')
+
+# Split train and test
+idx=pd.IndexSlice
+train_df=all_data.loc[idx[['train',],:]]
+test_df=all_data.loc[idx[['test',],:]]
+train_label=train['CarInsurance']
+print(train_df.shape)
+print(len(train_label))
+print(test_df.shape)
+
+# Train test split
+x_train, x_test, y_train, y_test = train_test_split(train_df,train_label,test_size = 0.3,random_state=3)
+
+
+print('\n\n3A + 4A')
+# Create a cross validation function 
+def get_best_model(estimator, params_grid={}):
+    model = GridSearchCV(estimator = estimator,param_grid = params_grid,cv=3, scoring="accuracy", n_jobs= -1)
+    model.fit(x_train,y_train)
+    print('\n--- Best Parameters -----------------------------')
+    print(model.best_params_)
+    print('\n--- Best Model -----------------------------')
+    best_model = model.best_estimator_
+    print(best_model)
+    return best_model
+
+# Create a model fitting function
+def model_fit(model,feature_imp=True,cv=5):
+
+    # model fit   
+    clf = model.fit(x_train,y_train)
+    
+    # model prediction     
+    y_pred = clf.predict(x_test)
+    
+    # model report     
+    cm = confusion_matrix(y_test,y_pred)
+    plot_confusion_matrix(cm, classes=class_names, title='Confusion matrix')
+
+    print('\n--- Train Set -----------------------------')
+    print('Accuracy: %.5f +/- %.4f' % (np.mean(cross_val_score(clf,x_train,y_train,cv=cv)),np.std(cross_val_score(clf,x_train,y_train,cv=cv))))
+    print('AUC: %.5f +/- %.4f' % (np.mean(cross_val_score(clf,x_train,y_train,cv=cv,scoring='roc_auc')),np.std(cross_val_score(clf,x_train,y_train,cv=cv,scoring='roc_auc'))))
+    print('\n--- Validation Set -----------------------------')    
+    print('Accuracy: %.5f +/- %.4f' % (np.mean(cross_val_score(clf,x_test,y_test,cv=cv)),np.std(cross_val_score(clf,x_test,y_test,cv=cv))))
+    print('AUC: %.5f +/- %.4f' % (np.mean(cross_val_score(clf,x_test,y_test,cv=cv,scoring='roc_auc')),np.std(cross_val_score(clf,x_test,y_test,cv=cv,scoring='roc_auc'))))
+    print('-----------------------------------------------') 
+
+    # feature importance 
+    if feature_imp:
+        feat_imp = pd.Series(clf.feature_importances_,index=all_data.columns)
+        feat_imp = feat_imp.nlargest(15).sort_values()
+        plt.figure()
+        feat_imp.plot(kind="barh",figsize=(6,8),title="Most Important Features")
+        
+        
+import itertools
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    bottom, top = plt.ylim()
+    plt.gca().set_ylim(bottom + 0.5, top - 0.5)
+
+class_names = ['Success','Failure']
+
+# We're making progress here. Logistic regression performs better than KNN. 
+lg = LogisticRegression(random_state=3)
+parameters = {'C':[0.8,0.9,1], 
+              'penalty':['l1','l2']}
+clf_lg = get_best_model(lg,parameters)
+model_fit(model=clf_lg, feature_imp=False)
+
+dt = DecisionTreeClassifier()
+parameters = {
+                'max_depth':[10],
+                'max_features':[13,14],
+                'min_samples_split':[11]}
+clf_dt = get_best_model(dt,parameters)
+model_fit(model=clf_lg, feature_imp=False)
+
+print('\n\n3B')
+# Compare model performance
+clfs= [clf_lg, clf_dt]
+index =['Logistic Regression','Decision Tree']
+scores=[]
+for clf in clfs:
+    score = np.mean(cross_val_score(clf,x_test,y_test,cv=5,scoring = 'accuracy'))
+    scores = np.append(scores,score)
+models = pd.Series(scores,index=index)
+models.sort_values(ascending=False)
+
+print('\nSo the Logistic Regression Model performs better')
